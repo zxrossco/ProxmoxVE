37c0fe7960b94541735d32c8c95947a271ad797a
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-11-08 16:16:21 +0100

    quickfix homarr.sh (#133)
    Missing Space

--- ct/homarr.sh
+++ ct/homarr.sh
@@ -55,7 +55,7 @@ function default_settings() {
 
 function update_script() {
 header_info
-if [[ ! -d /opt/homarr]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+if [[ ! -d /opt/homarr ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
 if (( $(df /boot | awk 'NR==2{gsub("%","",$5); print $5}') > 80 )); then
   read -r -p "Warning: Storage is dangerously low, continue anyway? <y/N> " prompt
   [[ ${prompt,,} =~ ^(y|yes)$ ]] || exit
