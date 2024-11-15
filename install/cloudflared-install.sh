f6cc26af3af94d3c15fd9e23c5b8e40086b20e97
davalanche <d.avalanche@outlook.com>
2024-11-15 11:15:25 -0600

    Update cloudflared-install.sh (#264)
    Added the option to configure cloudflared as a DNS-over-HTTPS (DoH) proxy

--- install/cloudflared-install.sh
+++ install/cloudflared-install.sh
@@ -28,6 +28,41 @@ $STD apt-get update
 $STD apt-get install -y cloudflared
 msg_ok "Installed Cloudflared"
 
+read -r -p "Would you like to configure cloudflared as a DNS-over-HTTPS (DoH) proxy? <y/N> " prompt
+if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
+  msg_info "Creating Service"
+  cat <<EOF >/usr/local/etc/cloudflared/config.yml
+proxy-dns: true
+proxy-dns-address: 0.0.0.0
+proxy-dns-port: 53
+proxy-dns-max-upstream-conns: 5
+proxy-dns-upstream:
+  - https://1.1.1.1/dns-query
+  - https://1.0.0.1/dns-query
+  #- https://8.8.8.8/dns-query
+  #- https://8.8.4.4/dns-query
+  #- https://9.9.9.9/dns-query
+  #- https://149.112.112.112/dns-query
+EOF
+  cat <<EOF >/etc/systemd/system/cloudflared.service
+[Unit]
+Description=cloudflared DNS-over-HTTPS (DoH) proxy
+After=syslog.target network-online.target
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/cloudflared --config /usr/local/etc/cloudflared/config.yml
+Restart=on-failure
+RestartSec=10
+KillMode=process
+
+[Install]
+WantedBy=multi-user.target
+EOF
+  systemctl enable -q --now cloudflared.service
+  msg_ok "Created Service"
+fi
+
 motd_ssh
 customize
 
