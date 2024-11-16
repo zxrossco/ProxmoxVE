bae79149f0bab548b8f253021f13a0f6e0bd28b1
Don Locke <75755573+DonLocke@users.noreply.github.com>
2024-11-16 03:02:57 -0700

    Add Wavelog Script (#259)
    * Add Wavelog Script
    
    * Added github logo which is less likely to disappear.
    
    * Add Copyright, remove zip, and simplify mv in install
    
    * Update core dependecies
    
    * Update json/wavelog.json
    
    Co-authored-by: Håvard Gjøby Thom <34199185+havardthom@users.noreply.github.com>
    
    * Remove old install during update
    
    ---------
    
    Co-authored-by: Paul <newzealandspaul@gmail.com>
    Co-authored-by: CanbiZ <47820557+MickLesk@users.noreply.github.com>
    Co-authored-by: Håvard Gjøby Thom <34199185+havardthom@users.noreply.github.com>

---
+++ ct/wavelog.sh
@@ -0,0 +1,109 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2024 community-scripts ORG
+# Author: Don Locke (DonLocke)
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
+function header_info {
+clear
+cat <<"EOF"
+ _       __                 __
+| |     / /___ __   _____  / /   ____  ____ _
+| | /| / / __ `/ | / / _ \/ /   / __ \/ __ '/
+| |/ |/ / /_/ /| |/ /  __/ /___/ /_/ / /_/ /
+|__/|__/\__,_/ |___/\___/_____/\____/\__, /
+                                    /____/
+EOF
+}
+header_info
+echo -e "Loading..."
+APP="Wavelog"
+var_disk="2"
+var_cpu="1"
+var_ram="512"
+var_os="debian"
+var_version="12"
+variables
+color
+catch_errors
+
+function default_settings() {
+  CT_TYPE="1"
+  PW=""
+  CT_ID=$NEXTID
+  HN=$NSAPP
+  DISK_SIZE="$var_disk"
+  CORE_COUNT="$var_cpu"
+  RAM_SIZE="$var_ram"
+  BRG="vmbr0"
+  NET="dhcp"
+  GATE=""
+  APT_CACHER=""
+  APT_CACHER_IP=""
+  DISABLEIP6="no"
+  MTU=""
+  SD=""
+  NS=""
+  MAC=""
+  VLAN=""
+  SSH="no"
+  VERB="no"
+  echo_default
+}
+
+function update_script() {
+header_info
+if [[ ! -d /opt/wavelog ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+RELEASE=$(curl -s https://api.github.com/repos/wavelog/wavelog/releases/latest | grep "tag_name" | cut -d '"' -f 4)
+if [[ ! -f /opt/${APP}_version.txt ]] || [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]]; then
+  msg_info "Stopping Services"
+  systemctl stop apache2
+  msg_ok "Services Stopped"
+
+  msg_info "Updating ${APP} to ${RELEASE}"
+  cp /opt/wavelog/application/config/config.php /opt/config.php
+  cp /opt/wavelog/application/config/database.php /opt/database.php
+  cp -r /opt/wavelog/userdata /opt/userdata
+  if [[ -f /opt/wavelog/assets/js/sections/custom.js ]]; then
+    cp /opt/wavelog/assets/js/sections/custom.js /opt/custom.js
+  fi
+  wget -q "https://github.com/wavelog/wavelog/archive/refs/tags/${RELEASE}.zip"
+  unzip -q ${RELEASE}.zip
+  rm -rf /opt/wavelog
+  mv wavelog-${RELEASE}/ /opt/wavelog
+  rm -rf /opt/wavelog/install
+  mv /opt/config.php /opt/wavelog/application/config/config.php
+  mv /opt/database.php /opt/wavelog/application/config/database.php
+  cp -r /opt/userdata/* /opt/wavelog/userdata
+  rm -rf /opt/userdata
+  if [[ -f /opt/custom.js ]]; then
+    mv /opt/custom.js /opt/wavelog/assets/js/sections/custom.js
+  fi
+  chown -R www-data:www-data /opt/wavelog/
+  find /opt/wavelog/ -type d -exec chmod 755 {} \;
+  find /opt/wavelog/ -type f -exec chmod 664 {} \;
+  echo "${RELEASE}" >/opt/${APP}_version.txt
+  msg_ok "Updated ${APP}"
+
+  msg_info "Starting Services"
+  systemctl start apache2
+  msg_ok "Started Services"
+
+  msg_info "Cleaning Up"
+  rm -rf ${RELEASE}.zip
+  msg_ok "Cleaned"
+  msg_ok "Updated Successfully"
+else
+  msg_ok "No update required. ${APP} is already at ${RELEASE}"
+fi
+exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${APP} should be reachable by going to the following URL.
+         ${BL}http://${IP}${CL} \n"
---
+++ install/wavelog-install.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2024 community-scripts ORG
+# Author: Don Locke (DonLocke)
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/wavelog/wavelog
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y \
+  curl \
+  libapache2-mod-php \
+  mariadb-server \
+  mc \
+  php8.2-{curl,mbstring,mysql,xml,zip} \
+  sudo \
+  unzip
+msg_ok "Installed Dependencies"
+
+msg_info "Setting up Database"
+DB_NAME=wavelog
+DB_USER=waveloguser
+DB_PASS=$(openssl rand -base64 18 | tr -dc 'a-zA-Z0-9' | head -c13)
+$STD mysql -u root -e "CREATE DATABASE $DB_NAME;"
+$STD mysql -u root -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED WITH mysql_native_password AS PASSWORD('$DB_PASS');"
+$STD mysql -u root -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'localhost'; FLUSH PRIVILEGES;"
+{
+    echo "Wavelog-Credentials"
+    echo "Wavelog Database User: $DB_USER"
+    echo "Wavelog Database Password: $DB_PASS"
+    echo "Wavelog Database Name: $DB_NAME"
+} >> ~/wavelog.creds
+msg_ok "Set up database"
+
+msg_info "Setting up PHP"
+sed -i '/max_execution_time/s/= .*/= 600/' /etc/php/8.2/apache2/php.ini
+sed -i '/memory_limit/s/= .*/= 256M/' /etc/php/8.2/apache2/php.ini
+sed -i '/upload_max_filesize/s/= .*/= 8M/' /etc/php/8.2/apache2/php.ini
+msg_ok "Set up PHP"
+
+msg_info "Installing Wavelog"
+RELEASE=$(curl -s https://api.github.com/repos/wavelog/wavelog/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
+wget -q "https://github.com/wavelog/wavelog/archive/refs/tags/${RELEASE}.zip"
+unzip -q ${RELEASE}.zip
+mv wavelog-${RELEASE}/ /opt/wavelog
+chown -R www-data:www-data /opt/wavelog/
+find /opt/wavelog/ -type d -exec chmod 755 {} \;
+find /opt/wavelog/ -type f -exec chmod 664 {} \;
+echo "${RELEASE}" >/opt/${APPLICATION}_version.txt
+msg_ok "Installed Wavelog"
+
+msg_info "Creating Service"
+cat <<EOF >/etc/apache2/sites-available/wavelog.conf
+<VirtualHost *:80>
+    ServerAdmin webmaster@localhost
+    DocumentRoot /opt/wavelog
+
+    <Directory /opt/wavelog>
+        Options Indexes FollowSymLinks
+        AllowOverride All
+        Require all granted
+    </Directory>
+
+    ErrorLog /var/log/apache2/error.log
+    CustomLog /var/log/apache2/access.log combined
+</VirtualHost>
+EOF
+$STD a2ensite wavelog.conf
+$STD a2dissite 000-default.conf  
+$STD systemctl reload apache2
+msg_ok "Created Service"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+rm -f ${RELEASE}.zip
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
