8f694c8e237433cff360c870eb0ed57fe8b561ef
cour64 <brendan.dlc@gmail.com>
2024-11-23 17:38:57 +0000

    Fix missing build tools for Actual Budget install (#455)

--- install/actualbudget-install.sh
+++ install/actualbudget-install.sh
@@ -14,11 +14,13 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apt-get install -y curl
-$STD apt-get install -y sudo
-$STD apt-get install -y mc
-$STD apt-get install -y gpg
-$STD apt-get install -y git
+$STD apt-get install -y \
+  curl \
+  sudo \
+  mc \
+  gpg \
+  git \
+  build-essential
 msg_ok "Installed Dependencies"
 
 msg_info "Setting up Node.js Repository"
