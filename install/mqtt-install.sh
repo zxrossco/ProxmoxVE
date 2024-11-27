9d72b5872af24d81c26423be692d30ac578b762a
Dominik Siebel <145283+dsiebel@users.noreply.github.com>
2024-11-27 15:54:07 +0100

    fix: install mosquitto from mosquitto repo (#534)

--- install/mqtt-install.sh
+++ install/mqtt-install.sh
@@ -21,6 +21,11 @@ $STD apt-get install -y gpg
 msg_ok "Installed Dependencies"
 
 msg_info "Installing Mosquitto MQTT Broker"
+source /etc/os-release
+curl -fsSL http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key >/usr/share/keyrings/mosquitto-repo.gpg.key
+chmod go+r /usr/share/keyrings/mosquitto-repo.gpg.key
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/mosquitto-repo.gpg.key] http://repo.mosquitto.org/debian ${VERSION_CODENAME} main" >/etc/apt/sources.list.d/mosquitto.list
+$STD apt-get update
 $STD apt-get -y install mosquitto
 $STD apt-get -y install mosquitto-clients
 cat <<EOF >/etc/mosquitto/conf.d/default.conf
