d5d5a3d3a34ad21f9dc5e273d2059f84e3936288
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-11-29 19:44:08 +0100

    switch copyright
    new script, correct copyright

--- install/the-lounge-install.sh
+++ install/the-lounge-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2021-2024 tteck
-# Author: tteck (tteckster)
+# Copyright (c) 2021-2024 community-scripts ORG
+# Author: kristocopani
 # License: MIT
 # https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 
