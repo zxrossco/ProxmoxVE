862b72ab39c4a84527763bbf5cf4e534505d9be2
Kristo Copani <31226503+quantumryuu@users.noreply.github.com>
2024-11-29 19:13:20 +0200

    New Script: The Lounge IRC (#571)
    * Added "thelounge.sh"
    
    * Added "thelounge-install.sh"
    
    * Added "thelounge.json"
    
    * Added empty line to end of thelounge.sh
    
    * Added empty line to end of thelounge.json
    
    * Update thelounge-install.sh
    
    * replace apt install with dpkg -i
    
    * cleanup
    
    * cleanup 2
    
    * header_info try
    
    * header_info fix1?
    
    * Fix header
    
    * Test 1
    
    * Update thelounge.sh
    
    * Update thelounge.sh
    
    * Update thelounge.sh
    
    * Update thelounge.sh
    
    * Update thelounge-install.sh
    
    * Update thelounge.json
    
    * Little Fixes
    
    * Update thelounge.sh
    
    ---------
    
    Co-authored-by: Kristo Copani <31226503+kristocopani@users.noreply.github.com>
    Co-authored-by: CanbiZ <47820557+MickLesk@users.noreply.github.com>

---
+++ ct/thelounge.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2024 community-scripts ORG
+# Author: kristocopani
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
+function header_info {
+clear
+cat <<"EOF"
+  ________            __                               
+ /_  __/ /_  ___     / /   ____  __  ______  ____ ____ 
+  / / / __ \/ _ \   / /   / __ \/ / / / __ \/ __ `/ _ \
+ / / / / / /  __/  / /___/ /_/ / /_/ / / / / /_/ /  __/
+/_/ /_/ /_/\___/  /_____/\____/\__,_/_/ /_/\__, /\___/ 
+                                          /____/       
+EOF
+}
+header_info
+echo -e "Loading..."
+APP="The Lounge"
+var_disk="4"
+var_cpu="2"
+var_ram="2048"
+var_os="ubuntu"
+var_version="24.04"
+variables
+color
+catch_errors
+
+function default_settings() {
+  CT_TYPE="1"
+  PW=""
+  CT_ID=$NEXTID
+  HN=$NSAPP
+  DISK_SIZE="$var_disk"
+  CORE_COUNT="$var_cpu"
+  RAM_SIZE="$var_ram"
+  BRG="vmbr0"
+  NET="dhcp"
+  GATE=""
+  APT_CACHER=""
+  APT_CACHER_IP=""
+  DISABLEIP6="no"
+  MTU=""
+  SD=""
+  NS=""
+  MAC=""
+  VLAN=""
+  SSH="no"
+  VERB="no"
+  echo_default
+}
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+
+  if [[ ! -f /usr/lib/systemd/system/thelounge.service ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+  RELEASE=$(curl -s https://api.github.com/repos/thelounge/thelounge-deb/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+  if [[ ! -f /opt/${APP}_version.txt ]] || [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]]; then
+    msg_info "Stopping Service"
+    systemctl stop thelounge
+    msg_ok "Stopped Service"
+
+    msg_info "Updating ${APP} to v${RELEASE}"
+    apt-get install --only-upgrade nodejs &>/dev/null
+    cd /opt
+    wget -q https://github.com/thelounge/thelounge-deb/releases/download/v${RELEASE}/thelounge_${RELEASE}_all.deb
+    dpkg -i ./thelounge_${RELEASE}_all.deb
+    msg_ok "Updated ${APP} to v${RELEASE}"
+
+    msg_info "Starting Service"
+    systemctl start thelounge
+    msg_ok "Started Service"
+
+    msg_info "Cleaning up"
+    rm -rf "/opt/thelounge_${RELEASE}_all.deb"
+    msg_ok "Cleaned"
+    msg_ok "Updated Successfully"
+  else
+    msg_ok "No update required.  ${APP} is already at v${RELEASE}."
+  fi
+  exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${APP} should be reachable by going to the following URL.
+         ${BL}http://${IP}:9000${CL} \n"
---
+++ install/thelounge-install.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2024 tteck
+# Author: kristocopani
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y \
+    curl \
+    wget \
+    mc
+msg_ok "Installed Dependencies"
+
+msg_info "Setting up Node.js Repository"
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
+msg_ok "Set up Node.js Repository"
+
+msg_info "Installing Node.js"
+$STD apt-get update
+$STD apt-get install -y nodejs
+$STD npm install --global yarn
+msg_ok "Installed Node.js"
+
+msg_info "Installing The Lounge"
+cd /opt
+RELEASE=$(curl -s https://api.github.com/repos/thelounge/thelounge-deb/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+wget -q https://github.com/thelounge/thelounge-deb/releases/download/v${RELEASE}/thelounge_${RELEASE}_all.deb
+$STD dpkg -i ./thelounge_${RELEASE}_all.deb
+echo "${RELEASE}" >"/opt/${APPLICATION}_version.txt"
+msg_ok "Installed The Lounge"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+rm -rf "/opt/thelounge_${RELEASE}_all.deb"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
