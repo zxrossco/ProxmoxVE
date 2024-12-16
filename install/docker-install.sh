9fc81e49e42bada7c338cf5939288525d47c3e65
Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
2024-12-16 14:29:51 +0100

    Update Script: Remove Docker Compose Question (#847)

--- install/docker-install.sh
+++ install/docker-install.sh
@@ -62,15 +62,6 @@ else
     msg_ok "Installed Portainer Agent $PORTAINER_AGENT_LATEST_VERSION"
   fi
 fi
-read -r -p "Would you like to add Docker Compose? <y/N> " prompt
-if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
-  msg_info "Installing Docker Compose $DOCKER_COMPOSE_LATEST_VERSION"
-  DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-  mkdir -p $DOCKER_CONFIG/cli-plugins
-  curl -sSL https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_LATEST_VERSION/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-  chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
-  msg_ok "Installed Docker Compose $DOCKER_COMPOSE_LATEST_VERSION"
-fi
 
 motd_ssh
 customize
