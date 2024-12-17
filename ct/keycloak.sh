870313cc58b86d1230d178133ab325de0e50357e
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-12-17 14:21:15 +0100

    Update keycloak.sh (#874)

--- ct/keycloak.sh
+++ ct/keycloak.sh
@@ -7,7 +7,7 @@ source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/m
 
 # App Default Values
 APP="Keycloak"
-var_tags="access management"
+var_tags="access-management"
 var_cpu="2"
 var_ram="2048"
 var_disk="4"
@@ -25,38 +25,38 @@ color
 catch_errors
 
 function update_script() {
-    header_info
-    check_container_storage
-    check_container_resources
-    if [[ ! -f /etc/systemd/system/keycloak.service ]]; then
-        msg_error "No ${APP} Installation Found!"
-        exit
-    fi
-    msg_info "Updating ${APP} LXC"
-
-    msg_info "Updating packages"
-    apt-get update &>/dev/null
-    apt-get -y upgrade &>/dev/null
-
-    RELEASE=$(curl -s https://api.github.com/repos/keycloak/keycloak/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
-    msg_info "Updating Keycloak to v$RELEASE"
-    cd /opt
-    wget -q https://github.com/keycloak/keycloak/releases/download/$RELEASE/keycloak-$RELEASE.tar.gz
-    mv keycloak keycloak.old
-    tar -xzf keycloak-$RELEASE.tar.gz
-    cp -r keycloak.old/conf keycloak-$RELEASE
-    cp -r keycloak.old/providers keycloak-$RELEASE
-    cp -r keycloak.old/themes keycloak-$RELEASE
-    mv keycloak-$RELEASE keycloak
-
-    msg_info "Delete temporary installation files"
-    rm keycloak-$RELEASE.tar.gz
-    rm -rf keycloak.old
-
-    msg_info "Restating Keycloak"
-    systemctl restart keycloak
-    msg_ok "Updated Successfully"
+  header_info
+  check_container_storage
+  check_container_resources
+  if [[ ! -f /etc/systemd/system/keycloak.service ]]; then
+    msg_error "No ${APP} Installation Found!"
     exit
+  fi
+  msg_info "Updating ${APP} LXC"
+
+  msg_info "Updating packages"
+  apt-get update &>/dev/null
+  apt-get -y upgrade &>/dev/null
+
+  RELEASE=$(curl -s https://api.github.com/repos/keycloak/keycloak/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
+  msg_info "Updating Keycloak to v$RELEASE"
+  cd /opt
+  wget -q https://github.com/keycloak/keycloak/releases/download/$RELEASE/keycloak-$RELEASE.tar.gz
+  mv keycloak keycloak.old
+  tar -xzf keycloak-$RELEASE.tar.gz
+  cp -r keycloak.old/conf keycloak-$RELEASE
+  cp -r keycloak.old/providers keycloak-$RELEASE
+  cp -r keycloak.old/themes keycloak-$RELEASE
+  mv keycloak-$RELEASE keycloak
+
+  msg_info "Delete temporary installation files"
+  rm keycloak-$RELEASE.tar.gz
+  rm -rf keycloak.old
+
+  msg_info "Restating Keycloak"
+  systemctl restart keycloak
+  msg_ok "Updated Successfully"
+  exit
 }
 
 start
