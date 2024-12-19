23d7587f04689911e53a4ffcd50c13c6d8a5f5d6
Maximilian Bosche <max@enten.space>
2024-12-19 15:09:19 +0100

    mysql not showing ip after install (#924)

--- ct/mysql.sh
+++ ct/mysql.sh
@@ -46,4 +46,4 @@ description
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW} Access it using the following IP:${CL}"
-echo -e "${TAB}${GATEWAY}${BGN}{IP}:3306${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}${IP}:3306${CL}"
