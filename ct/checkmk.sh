2326e1169923453ade0852a5106b0fc1e0977c98
Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
2024-12-20 10:01:32 +0100

    New Script: CheckMk (#926)
    * New Script: CheckMk
    
    * Add sudo
    
    * Change to dpkg -i

---
+++ ct/checkmk.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2024 community-scripts ORG
+# Author: Michel Roegl-Brunner (michelroegl-brunner)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://checkmk.com/
+
+APP="checkmk"
+var_tags="monitoring"
+var_cpu="2"
+var_ram="2048"
+var_disk="4"
+var_os="debian"
+var_version="12"
+var_unprivileged="1"
+
+header_info "$APP"
+base_settings
+
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+  if [[ ! -f /opt/checkmk_version.txt ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+  RELEASE=$(curl -fsSL https://api.github.com/repos/checkmk/checkmk/tags | grep "name" | awk '{print substr($2, 3, length($2)-4) }' | grep -v "*-rc" | tail -n +2 | head -n 1)
+  if [[ ! -f /opt/${APP}_version.txt ]] || [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]]; then
+    msg_info "Updating ${APP} to v${RELEASE}"
+    omd stop monitoring &>/dev/null
+    omd cp monitoring monitoringbackup &>/dev/null
+    wget -q https://download.checkmk.com/checkmk/${RELEASE}/check-mk-raw-${RELEASE}_0.bookworm_amd64.deb -O /opt/checkmk.deb
+    dpkg -i /opt/checkmk.deb &>/dev/null
+    omd --force -V ${RELEASE}.cre update --conflict=install monitoring &>/dev/null
+    omd start monitoring &>/dev/null
+    omd -f rm monitoringbackup  &>/dev/null
+    omd cleanup &>/dev/null
+    rm -rf /opt/checkmk.deb
+    msg_ok "Updated ${APP} to v${RELEASE}"
+  else
+    msg_ok "No update required. ${APP} is already at v${RELEASE}."
+  fi
+
+  exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}/monitoring${CL}"
---
+++ install/checkmk-install.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+#Copyright (c) 2021-2024 community-scripts ORG
+# Author: Michel Roegl-Brunner (michelroegl-brunner)
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y \
+  curl \
+  sudo \
+  mc 
+msg_ok "Installed Dependencies"
+
+msg_info "Install Checkmk"
+RELEASE=$(curl -fsSL https://api.github.com/repos/checkmk/checkmk/tags | grep "name" | awk '{print substr($2, 3, length($2)-4) }' | grep -v "*-rc" | tail -n +2 | head -n 1)
+wget -q https://download.checkmk.com/checkmk/${RELEASE}/check-mk-raw-${RELEASE}_0.bookworm_amd64.deb -O /opt/checkmk.deb
+$STD dpkg -i /opt/checkmk.deb
+echo "${RELEASE}" >"/opt/checkmk_version.txt"
+msg_ok "Installed Checkmk"
+
+msg_info "Creating Service"
+PASSWORD=$(omd create monitoring | grep "password:" | awk '{print $NF}')
+$STD omd start &>/dev/null 
+{
+    echo "Application-Credentials"
+    echo "Username: cmkadmin"
+    echo "Password: $PASSWORD"
+} >> ~/checkmk.creds
+msg_ok "Created Service"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+rm -rf /opt/checkmk.deb
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
