b0a523aac3601340ee62747f4d0089b1b1d9e818
Fraefel <thomas@fcon.dk>
2024-12-21 11:24:51 -0500

    Update homeassistant-core.sh (#961)
    Corrected the port number to 8123

--- ct/homeassistant-core.sh
+++ ct/homeassistant-core.sh
@@ -128,4 +128,4 @@ description
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW} Access it using the following URL:${CL}"
-echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8132${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8123${CL}"
