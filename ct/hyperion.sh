ae67ee8768ca4314bb8cc83906e76793b1e5c690
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-12-23 13:29:39 +0100

    Bugfix Tag in Hyperion CT

--- ct/hyperion.sh
+++ ct/hyperion.sh
@@ -7,7 +7,7 @@ source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/m
 
 # App Default Values
 APP="Hyperion"
-var_tags="ambient lightning"
+var_tags="ambient-lightning"
 var_cpu="1"
 var_ram="512"
 var_disk="2"
