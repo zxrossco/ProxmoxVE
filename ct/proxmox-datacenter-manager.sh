7aca36a3f0a37d62de9797964d4c3a6895da388c
Ryan Bradley <70593983+rbradley0@users.noreply.github.com>
2024-12-26 02:52:52 -0500

    Fix Script: Fix broken build.func url (#1013)

--- ct/proxmox-datacenter-manager.sh
+++ ct/proxmox-datacenter-manager.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/raw/main/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2024 community-scripts ORG
 # Author: CrazyWolf13
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
