d4c95779bf8d713f234046605e9074313a6a9d75
Johanna Rührig <TheRealVira@users.noreply.github.com>
2024-12-28 20:31:25 +0100

    Added missing port to access url (#1065)

--- ct/authentik.sh
+++ ct/authentik.sh
@@ -82,4 +82,4 @@ description
 
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW} Access it using the following URL:${CL}"
-echo -e "${TAB}${GATEWAY}${BGN}http://${IP}/if/flow/initial-setup/${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:9000/if/flow/initial-setup/${CL}"
