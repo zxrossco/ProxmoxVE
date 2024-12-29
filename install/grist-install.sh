024480d0a4397bfc41c0e7db28d6903cf41978bc
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-12-29 12:33:31 +0100

    some bugfixes

--- install/grist-install.sh
+++ install/grist-install.sh
@@ -29,14 +29,10 @@ msg_info "Installing Node.js"
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
-
 $STD apt-get update
 $STD apt-get install -y nodejs
-msg_ok "Installed Node.js"
-
-msg_info "Installing yarn"
 $STD npm install -g yarn
-msg_ok "Installed yarn $(yarn --version)"
+msg_ok "Installed Node.js"
 
 msg_info "Installing Grist"
 RELEASE=$(curl -s https://api.github.com/repos/gristlabs/grist-core/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
@@ -45,18 +41,17 @@ wget -q https://github.com/gristlabs/grist-core/archive/refs/tags/v${RELEASE}.zi
 unzip -q v$RELEASE.zip
 mv grist-core-${RELEASE} grist
 cd grist
-$STD yarn install
-$STD yarn run build:prod
-$STD yarn run install:python
-echo "${RELEASE}" >/opt/${APPLICATION}_version.txt
-
+yarn install
+yarn run build:prod
+yarn run install:python
 cat <<EOF >/opt/grist/.env
 NODE_ENV=production
 GRIST_HOST=0.0.0.0
 EOF
-
+echo "${RELEASE}" >/opt/${APPLICATION}_version.txt
 msg_ok "Installed Grist"
 
+msg_info "Create Service"
 cat <<EOF >/etc/systemd/system/grist.service
 [Unit]
 Description=Grist
