69ae09346f1e866fe29233633893379c200fabb1
Suraj Subramanian <suraj.tf6@gmail.com>
2024-12-31 13:25:53 +0530

    Update URL to https (#1124)

--- ct/netbox.sh
+++ ct/netbox.sh
@@ -87,4 +87,4 @@ description
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW} Access it using the following URL:${CL}"
-echo -e "${TAB}${GATEWAY}${BGN}http://${IP}${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}https://${IP}${CL}"
