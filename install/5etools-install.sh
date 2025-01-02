3b8ff01385cc21be38033c5cd2846468fafa10a0
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-01-02 20:59:51 +0100

    Update 5etools-install.sh

--- install/5etools-install.sh
+++ install/5etools-install.sh
@@ -24,23 +24,23 @@ $STD apt-get install -y \
 msg_ok "Installed Dependencies"
 
 # Setup App
-msg_info "Set up base 5etools"
+msg_info "Set up 5etools Base"
 RELEASE=$(curl -s https://api.github.com/repos/5etools-mirror-3/5etools-src/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
 wget -q "https://github.com/5etools-mirror-3/5etools-src/archive/refs/tags/${RELEASE}.zip"
 unzip -q "${RELEASE}.zip"
 mv "5etools-src-${RELEASE:1}" /opt/5etools
 echo "${RELEASE}" >"/opt/5etools_version.txt"
 rm "${RELEASE}.zip"
-msg_ok "Set up base 5etools"
+msg_ok "Set up 5etools Base"
 
-msg_info "Set up 5etools images"
+msg_info "Set up 5etools Image"
 IMG_RELEASE=$(curl -s https://api.github.com/repos/5etools-mirror-2/5etools-img/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
 curl -sSL "https://github.com/5etools-mirror-2/5etools-img/archive/refs/tags/${IMG_RELEASE}.zip" > "${IMG_RELEASE}.zip"
 unzip -q "${IMG_RELEASE}.zip"
 mv "5etools-img-${IMG_RELEASE:1}" /opt/5etools/img
 echo "${IMG_RELEASE}" >"/opt/5etools_IMG_version.txt"
 rm "${IMG_RELEASE}.zip"
-msg_ok "Set up 5etools images"
+msg_ok "Set up 5etools Image"
 
 msg_info "Creating Service"
 cat <<EOF >> /etc/apache2/apache2.conf
@@ -52,12 +52,10 @@ cat <<EOF >> /etc/apache2/apache2.conf
 EOF
 rm -rf /var/www/html
 ln -s "/opt/5etools" /var/www/html
-
 chown -R www-data: "/opt/5etools"
 chmod -R 755 "/opt/5etools"
-apache2ctl start
-msg_ok "Creating Service"
-# Cleanup
+msg_ok "Created Service"
+
 msg_info "Cleaning up"
 $STD apt-get -y autoremove
 $STD apt-get -y autoclean
