774cdcaf8d93ca155fe94841f9ad1e01a67a6438
tlissak <tlissak@gmail.com>
2025-01-06 15:25:53 +0100

    New Script: Typesense (#1291)
    * Add typesense database lxc container
    
    * fully upgrade typesende nstall
    
    * Update typesense.sh
    
    * Update typesense.json
    
    ---------
    
    Co-authored-by: CanbiZ <47820557+MickLesk@users.noreply.github.com>

---
+++ install/typesense-install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: tlissak
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://typesense.org/
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y \
+    curl \
+    mc \
+    sudo 
+msg_ok "Installed Dependencies"
+
+msg_info "Installing TypeSense"
+RELEASE=$(curl -s https://api.github.com/repos/typesense/typesense/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+cd /opt
+wget -q -O https://dl.typesense.org/releases/${RELEASE}/typesense-server-${RELEASE}-amd64.deb
+$STD apt install -y /opt/typesense-server-${RELEASE}-amd64.deb
+echo 'enable-cors = true' >> /etc/typesense/typesense-server.ini
+echo "${RELEASE}" >"/opt/${APPLICATION}_version.txt"
+msg_ok "Installed TypeSense"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+rm -rf /opt/typesense-server-${RELEASE}-amd64.deb
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
