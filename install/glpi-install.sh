5196539d1be241e75368518c9c58a2eb3b9f4e26
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-01-06 15:27:46 +0100

    fix glpi echo release

--- install/glpi-install.sh
+++ install/glpi-install.sh
@@ -46,10 +46,10 @@ msg_info "Installing GLPi"
 cd /opt
 RELEASE=$(curl -s https://api.github.com/repos/glpi-project/glpi/releases/latest | grep '"tag_name"' | sed -E 's/.*"tag_name": "([^"]+)".*/\1/')
 wget -q "https://github.com/glpi-project/glpi/releases/download/${RELEASE}/glpi-${RELEASE}.tgz"
-tar -xzvf glpi-${RELEASE}.tgz
+$STD tar -xzvf glpi-${RELEASE}.tgz
 cd /opt/glpi
 $STD php bin/console db:install --db-name=$DB_NAME --db-user=$DB_USER --db-password=$DB_PASS --no-interaction
-echo "${RELEASE}" >"/opt/${APP}_version.txt"
+echo "${RELEASE}" >/opt/${APPLICATION}_version.txt
 msg_ok "Installed GLPi"
 
 msg_info "Setting Downstream file"
