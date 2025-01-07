bc702e2a6da6b1eb31e19c10f4a6fab2a011b499
Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
2025-01-07 08:49:44 +0100

    Fix Script: Zammad (#1309)

--- ct/zammad.sh
+++ ct/zammad.sh
@@ -28,7 +28,7 @@ function update_script() {
     header_info
     check_container_storage
     check_container_resources
-    if [[ ! -d /opt/zamad ]]; then
+    if [[ ! -d /opt/zammad ]]; then
         msg_error "No ${APP} Installation Found!"
         exit
     fi
