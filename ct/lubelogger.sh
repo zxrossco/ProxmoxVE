26d73317e82c0129d17d02a2feecccf464a967f4
Kyle Kroboth <kroboth.k@gmail.com>
2025-01-11 01:36:23 -0500

    LubeLogger CT vehicle tag typo fix (#1413)

--- ct/lubelogger.sh
+++ ct/lubelogger.sh
@@ -7,7 +7,7 @@ source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/m
 
 # App Default Values
 APP="LubeLogger"
-var_tags="verhicle;car"
+var_tags="vehicle;car"
 var_cpu="1"
 var_ram="512"
 var_disk="2"
