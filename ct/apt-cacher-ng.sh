8cb63aa07ca4c3c3190897487644b88bdf1e305e
Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
2025-01-17 19:43:48 +0100

    Update apt-cacher-ng.sh: Typo (#1545)

--- ct/apt-cacher-ng.sh
+++ ct/apt-cacher-ng.sh
@@ -46,4 +46,4 @@ description
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW} Access it using the following URL:${CL}"
-echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:3142/acng-report.html{CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:3142/acng-report.html${CL}"
