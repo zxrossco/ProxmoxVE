79dfb7c00152969629f418080139dfb2b767772a
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-01-20 08:50:38 +0100

    Update pocketbase.sh (#1597)

--- ct/pocketbase.sh
+++ ct/pocketbase.sh
@@ -54,4 +54,4 @@ description
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW} Access it using the following URL:${CL}"
-echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8080/_${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8080/_/${CL}"
