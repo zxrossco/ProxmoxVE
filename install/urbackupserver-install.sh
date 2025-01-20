b8ed5e39877b51f615546f5826049342febd3b4c
Kristian Skov <kris701kj@gmail.com>
2025-01-20 14:02:43 +0100

    New Script: UrBackup Server (#1569)
    * Added UrBackup script
    
    * Fixed icon
    
    * Update urbackupserver-install.sh
    
    * Update install/urbackupserver-install.sh
    
    Co-authored-by: Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
    
    * Update install/urbackupserver-install.sh
    
    Co-authored-by: Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
    
    * Update json/urbackupserver.json
    
    Co-authored-by: Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
    
    * Changed from ubuntu to debian
    
    * Update urbackupserver.json
    
    ---------
    
    Co-authored-by: Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
    Co-authored-by: CanbiZ <47820557+MickLesk@users.noreply.github.com>

---
+++ install/urbackupserver-install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: Kristian Skov
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y \
+  curl \
+  sudo \
+  mc \
+  gnupg \
+  coreutils
+msg_ok "Installed Dependencies"
+
+msg_info "Installing UrBackup Server"
+curl -fsSL https://download.opensuse.org/repositories/home:uroni/Debian_12/Release.key | gpg --dearmor >/etc/apt/trusted.gpg.d/home_uroni.gpg
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/home_uroni.gpg] http://download.opensuse.org/repositories/home:/uroni/Debian_12/ /' >/etc/apt/sources.list.d/home:uroni.list
+$STD apt-get update -y
+apt-get install -y -qq urbackup-server
+msg_ok "Installed UrBackup Server"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
