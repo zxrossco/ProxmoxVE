fb67025f3b90b8cf362d7171aece047d26645495
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-01-20 14:44:34 +0100

    Update kimai-install.sh

--- install/kimai-install.sh
+++ install/kimai-install.sh
@@ -27,15 +27,15 @@ $STD apt-get install -y \
   lsb-release
 msg_ok "Installed Dependencies"
 
-#msg_info "Setup PHP8.4 Repository"
-#$STD curl -sSLo /tmp/debsuryorg-archive-keyring.deb https://packages.sury.org/debsuryorg-archive-keyring.deb
-#$STD dpkg -i /tmp/debsuryorg-archive-keyring.deb
-#$STD sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
-#$STD apt-get update
-#msg_ok "Setup PHP8.4 Repository"
+msg_info "Setup PHP8.4 Repository"
+$STD curl -sSLo /tmp/debsuryorg-archive-keyring.deb https://packages.sury.org/debsuryorg-archive-keyring.deb
+$STD dpkg -i /tmp/debsuryorg-archive-keyring.deb
+$STD sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+$STD apt-get update
+msg_ok "Setup PHP8.4 Repository"
 
 msg_info "Setup PHP"
-#$STD apt-get remove -y php8.2*
+$STD apt-get remove -y php8.2*
 #$STD apt-get remove -y php8.3*
 $STD apt-get install -y \
   php8.3 \
