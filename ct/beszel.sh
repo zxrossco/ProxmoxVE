038947416d347f651300c7aa60be8b93154bab01
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-01-21 11:07:09 +0100

    add port for beszel

--- ct/beszel.sh
+++ ct/beszel.sh
@@ -44,4 +44,4 @@ description
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW} Access it using the following IP:${CL}"
-echo -e "${TAB}${GATEWAY}${BGN}http://${IP}${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8090${CL}"
