2f2d1eeec3e8e789b6cf03f94186a354bfed879f
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-01-21 16:13:59 +0100

    Update actualbudget-install.sh

--- install/actualbudget-install.sh
+++ install/actualbudget-install.sh
@@ -38,8 +38,8 @@ msg_ok "Installed Node.js"
 
 msg_info "Installing Actual Budget"
 RELEASE=$(curl -s https://api.github.com/repos/actualbudget/actual-server/tags | jq --raw-output '.[0].name')
-wget -q https://github.com/actualbudget/actual-server/archive/refs/tags/${RELEASE}.tar.gz
-$STD tar -xzvf ${RELEASE}.tar.gz 
+wget -q https://codeload.github.com/actualbudget/actual-server/legacy.tar.gz/refs/tags/${RELEASE} -O actual-server.tar.gz
+$STD tar -xzvf actual-server.tar.gz
 mv *ctual-server-* /opt/actualbudget
 mkdir -p /opt/actualbudget/server-files
 mkdir -p /opt/actualbudget-data
