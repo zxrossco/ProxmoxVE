24dfa9590edb29965026c507c9e4093a1c418db1
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-01-28 21:13:21 +0100

    Aktualisieren von homarr.sh

--- ct/homarr.sh
+++ ct/homarr.sh
@@ -9,7 +9,7 @@ source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/m
 APP="Homarr"
 var_tags="arr;dashboard"
 var_cpu="2"
-var_ram="2048"
+var_ram="4096"
 var_disk="8"
 var_os="debian"
 var_version="12"
