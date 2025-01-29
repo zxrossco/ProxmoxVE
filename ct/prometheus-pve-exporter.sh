63e3619b68641f468036a48f0e8ca9c9f7b441fe
Andy Grunwald <andygrunwald@gmail.com>
2025-01-29 18:41:44 +0100

    New Script: Prometheus Proxmox VE Exporter (#1805)
    * New Script: Prometheus Proxmox VE Exporter
    
    See https://github.com/prometheus-pve/prometheus-pve-exporter
    
    * Fix formatting of config file output after installation
    
    * Prometheus Proxmox VE Exporter: Set category 1 for Website
    
    * Removed blank lines
    
    * Remove `python3-dev`, as it was commented out
    
    * Prometheus Proxmox VE Exporter: Move configuration under /opt
    
    * Prometheus Proxmox VE Exporter: Add logo
    
    * Change `msg_info "Updating Python3"` to `msg_info "Setup Python3"`
    
    * Removed empty line
    
    * Add note about Proxmox credentials into the website
    
    * Temp: Replace GitHub URLs to test my fork
    
    * Set correct `APP="Prometheus-PVE-Exporter"`
    
    * Add `--root-user-action=ignore` to pip call (as root)
    
    * Revert "Temp: Replace GitHub URLs to test my fork"
    
    This reverts commit 2308d483763add499c49d874f8f35e3240f1c0f7.

---
+++ ct/prometheus-pve-exporter.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: Andy Grunwald (andygrunwald)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/prometheus-pve/prometheus-pve-exporter
+
+# App Default Values
+APP="Prometheus-PVE-Exporter"
+var_tags="monitoring"
+var_cpu="1"
+var_ram="1024"
+var_disk="2"
+var_os="debian"
+var_version="12"
+var_unprivileged="1"
+
+# App Output & Base Settings
+header_info "$APP"
+base_settings
+
+# Core
+variables
+color
+catch_errors
+
+function update_script() {
+    header_info
+    check_container_storage
+    check_container_resources
+    if [[ ! -f /etc/systemd/system/prometheus-pve-exporter.service ]]; then
+        msg_error "No ${APP} Installation Found!"
+        exit
+    fi
+    msg_info "Stopping ${APP}"
+    systemctl stop prometheus-pve-exporter
+    msg_ok "Stopped ${APP}"
+
+    msg_info "Updating ${APP}"
+    pip install prometheus-pve-exporter --upgrade --root-user-action=ignore &>/dev/null
+    msg_ok "Updated ${APP}"
+
+    msg_info "Starting ${APP}"
+    systemctl start prometheus-pve-exporter
+    msg_ok "Started ${APP}"
+    msg_ok "Updated Successfully"
+    exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:9221${CL}"
---
+++ install/prometheus-pve-exporter-install.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: Andy Grunwald (andygrunwald)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/prometheus-pve/prometheus-pve-exporter
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y \
+  curl \
+  sudo \
+  mc
+msg_ok "Installed Dependencies"
+
+msg_info "Setup Python3"
+$STD apt-get install -y \
+  python3 \
+  python3-pip
+rm -rf /usr/lib/python3.*/EXTERNALLY-MANAGED
+msg_ok "Setup Python3"
+
+msg_info "Installing Prometheus Proxmox VE Exporter"
+python3 -m pip install --quiet --root-user-action=ignore prometheus-pve-exporter
+mkdir -p /opt/prometheus-pve-exporter
+cat <<EOF > /opt/prometheus-pve-exporter/pve.yml
+default:
+    user: prometheus@pve
+    password: sEcr3T!
+    verify_ssl: false
+EOF
+msg_ok "Installed Prometheus Proxmox VE Exporter"
+
+msg_info "Creating Service"
+cat <<EOF >/etc/systemd/system/prometheus-pve-exporter.service
+[Unit]
+Description=Prometheus Proxmox VE Exporter
+Documentation=https://github.com/znerol/prometheus-pve-exporter
+After=syslog.target network.target
+
+[Service]
+User=root
+Restart=always
+Type=simple
+ExecStart=pve_exporter \
+    --config.file=/opt/prometheus-pve-exporter/pve.yml \
+    --web.listen-address=0.0.0.0:9221
+ExecReload=/bin/kill -HUP \$MAINPID
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable -q --now prometheus-pve-exporter
+msg_ok "Created Service"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
