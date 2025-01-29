139f84a934bc39c394e053fdc3d41c1e2e19fc50
Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
2025-01-29 19:07:31 +0100

    [Frontend] Add /data to show API results (#1841)
    * [Frontend] Add /data to show API results
    
    * [Frontend] Add /data to show API results
    
    * update page.tsx
    
    * update page.tsx
    
    * update page.tsx
    
    * update page.tsx

---
+++ frontend/src/app/data/page.tsx
@@ -0,0 +1,236 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { string } from "zod";
+
+
+interface DataModel {
+  id: number;
+  ct_type: number;
+  disk_size: number;
+  core_count: number;
+  ram_size: number;
+  verbose: string;
+  os_type: string;
+  os_version: string;
+  hn: string;
+  disableip6: string;
+  ssh: string;
+  tags: string;
+  nsapp: string;
+  created_at: string;
+  method: string;
+  pve_version: string;
+}
+
+
+const DataFetcher: React.FC = () => {
+  const [data, setData] = useState<DataModel[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
+  const [sortConfig, setSortConfig] = useState<{ key: keyof DataModel | null, direction: 'ascending' | 'descending' }>({ key: 'id', direction: 'descending' });
+  const [itemsPerPage, setItemsPerPage] = useState(5);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://api.htl-braunau.at/data/json");
+        if (!response.ok) throw new Error("Failed to fetch data: ${response.statusText}");
+        const result: DataModel[] = await response.json();
+        setData(result);
+      } catch (err) {
+        setError((err as Error).message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
+  const filteredData = data.filter(item => {
+    const matchesSearchQuery = Object.values(item).some(value =>
+      value.toString().toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    const itemDate = new Date(item.created_at);
+    const matchesDateRange = (!startDate || itemDate >= startDate) && (!endDate || itemDate <= endDate);
+    return matchesSearchQuery && matchesDateRange;
+  });
+
+  const sortedData = React.useMemo(() => {
+    let sortableData = [...filteredData];
+    if (sortConfig.key !== null) {
+      sortableData.sort((a, b) => {
+        if (sortConfig.key !== null && a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? -1 : 1;
+        }
+        if (sortConfig.key !== null && a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+    return sortableData;
+  }, [filteredData, sortConfig]);
+
+  const requestSort = (key: keyof DataModel | null) => {
+    let direction: 'ascending' | 'descending' = 'ascending';
+    if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+      direction = 'descending';
+    } else if (sortConfig.key === key && sortConfig.direction === 'descending') {
+      direction = 'ascending';
+    } else {
+      direction = 'descending';
+    }
+    setSortConfig({ key, direction });
+  };
+
+  interface SortConfig {
+    key: keyof DataModel | null;
+    direction: 'ascending' | 'descending';
+  }
+
+  const formatDate = (dateString: string): string => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const timezoneOffset = dateString.slice(-6);
+    return `${day}.${month}.${year} ${hours}:${minutes} ${timezoneOffset} GMT`;
+  };
+
+  const handleItemsPerPageChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setItemsPerPage(Number(event.target.value));
+    setCurrentPage(1);
+  };
+
+  const paginatedData = sortedData.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+
+  return (
+    <div className="p-6 mt-20">
+      <h1 className="text-2xl font-bold mb-4 text-center">Created LXCs</h1>
+      <div className="mb-4 flex space-x-4">
+        <div>
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchQuery}
+            onChange={e => setSearchQuery(e.target.value)}
+            className="p-2 border"
+          />
+          <label className="text-sm text-gray-600 mt-1 block">Search by keyword</label>
+        </div>
+        <div>
+          <DatePicker
+            selected={startDate}
+            onChange={date => setStartDate(date)}
+            selectsStart
+            startDate={startDate}
+            endDate={endDate}
+            placeholderText="Start date"
+            className="p-2 border"
+          />
+          <label className="text-sm text-gray-600 mt-1 block">Set a start date</label>
+        </div>
+
+        <div>
+          <DatePicker
+            selected={endDate}
+            onChange={date => setEndDate(date)}
+            selectsEnd
+            startDate={startDate}
+            endDate={endDate}
+            placeholderText="End date"
+            className="p-2 border"
+          />
+          <label className="text-sm text-gray-600 mt-1 block">Set a end date</label>
+        </div>
+      </div>
+      <div className="mb-4 flex justify-between items-center">
+        <p className="text-lg font-bold">{filteredData.length} results found</p>
+        <select value={itemsPerPage} onChange={handleItemsPerPageChange} className="p-2 border">
+          <option value={5}>5</option>
+          <option value={10}>10</option>
+          <option value={20}>20</option>
+          <option value={50}>50</option>
+        </select>
+      </div>
+      <div className="overflow-x-auto">
+        <div className="overflow-y-auto lg:overflow-y-visible">
+          <table className="min-w-full table-auto border-collapse">
+            <thead>
+              <tr>
+                <th className="px-4 py-2 border-b cursor-pointer" onClick={() => requestSort('nsapp')}>Application</th>
+                <th className="px-4 py-2 border-b cursor-pointer" onClick={() => requestSort('os_type')}>OS</th>
+                <th className="px-4 py-2 border-b cursor-pointer" onClick={() => requestSort('os_version')}>OS Version</th>
+                <th className="px-4 py-2 border-b cursor-pointer" onClick={() => requestSort('disk_size')}>Disk Size</th>
+                <th className="px-4 py-2 border-b cursor-pointer" onClick={() => requestSort('core_count')}>Core Count</th>
+                <th className="px-4 py-2 border-b cursor-pointer" onClick={() => requestSort('ram_size')}>RAM Size</th>
+                <th className="px-4 py-2 border-b cursor-pointer" onClick={() => requestSort('hn')}>Hostname</th>
+                <th className="px-4 py-2 border-b cursor-pointer" onClick={() => requestSort('ssh')}>SSH</th>
+                <th className="px-4 py-2 border-b cursor-pointer" onClick={() => requestSort('verbose')}>Verb</th>
+                <th className="px-4 py-2 border-b cursor-pointer" onClick={() => requestSort('tags')}>Tags</th>
+                <th className="px-4 py-2 border-b cursor-pointer" onClick={() => requestSort('method')}>Method</th>
+                <th className="px-4 py-2 border-b cursor-pointer" onClick={() => requestSort('pve_version')}>PVE Version</th>
+                <th className="px-4 py-2 border-b cursor-pointer" onClick={() => requestSort('created_at')}>Created At</th>
+              </tr>
+            </thead>
+            <tbody>
+              {paginatedData.map((item, index) => (
+                <tr key={index}>
+                  <td className="px-4 py-2 border-b">{item.nsapp}</td>
+                  <td className="px-4 py-2 border-b">{item.os_type}</td>
+                  <td className="px-4 py-2 border-b">{item.os_version}</td>
+                  <td className="px-4 py-2 border-b">{item.disk_size}</td>
+                  <td className="px-4 py-2 border-b">{item.core_count}</td>
+                  <td className="px-4 py-2 border-b">{item.ram_size}</td>
+                  <td className="px-4 py-2 border-b">{item.hn}</td>
+                  <td className="px-4 py-2 border-b">{item.ssh}</td>
+                  <td className="px-4 py-2 border-b">{item.verbose}</td>
+                  <td className="px-4 py-2 border-b">{item.tags.replace(/;/g, ' ')}</td>
+                  <td className="px-4 py-2 border-b">{item.method}</td>
+                  <td className="px-4 py-2 border-b">{item.pve_version}</td>
+                  <td className="px-4 py-2 border-b">{formatDate(item.created_at)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="mt-4 flex justify-between items-center">
+        <button
+          onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
+          disabled={currentPage === 1}
+          className="p-2 border"
+        >
+          Previous
+        </button>
+        <span>Page {currentPage}</span>
+        <button
+          onClick={() => setCurrentPage(prev => (prev * itemsPerPage < sortedData.length ? prev + 1 : prev))}
+          disabled={currentPage * itemsPerPage >= sortedData.length}
+          className="p-2 border"
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+
+
+export default DataFetcher;
