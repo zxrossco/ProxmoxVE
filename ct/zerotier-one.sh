a6f656eacfcd07c0a8d3e55cac01cb720dcfb8b1
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-02-05 15:39:23 +0100

    New Script: Zerotier Controller (#1928)
    * add zerotier script
    
    * Update ct/zerotier-one.sh
    
    Co-authored-by: Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
    
    * Update ct/zerotier-one.sh
    
    Co-authored-by: Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
    
    * Update ct/zerotier-one.sh
    
    Co-authored-by: Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
    
    * Update install/zerotier-one-install.sh
    
    Co-authored-by: Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
    
    * Update install/zerotier-one-install.sh
    
    Co-authored-by: Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>
    
    * remove sleep
    
    * improvements to install procedure
    
    * fixes
    
    ---------
    
    Co-authored-by: Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>

---
+++ ct/zerotier-one.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: tremor021
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
+
+# App Default Values
+APP="Zerotier-One"
+var_tags="networking"
+var_cpu="1"
+var_ram="512"
+var_disk="4"
+var_os="debian"
+var_version="12"
+var_unprivileged="1"
+
+# App Output & Base Settings
+header_info "$APP"
+base_settings
+
+# Core
+variables
+color
+catch_errors
+
+function update_script() {
+    header_info
+    check_container_storage
+    check_container_resources
+
+    if [[ ! -f /usr/sbin/zerotier-one ]]; then
+        msg_error "No ${APP} Installation Found!"
+        exit
+    fi
+    msg_info "Stopping Service"
+    systemctl stop zerotier-one
+    msg_ok "Stopping Service"
+    msg_info "Updating ${APP}"
+    apt-get update &>/dev/null
+    apt-get -y upgrade
+    msg_ok "Updated ${APP}"
+
+    msg_info "Starting Service"
+    systemctl start zerotier-one
+    msg_ok "Started Service"
+    exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following IP:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}https://${IP}:3443${CL}"
---
+++ install/zerotier-one-install.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+#Copyright (c) 2021-2025 community-scripts ORG
+# Author: tremor021
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y \
+  curl \
+  mc \
+  sudo
+msg_ok "Installed Dependencies"
+
+msg_info "Setting up Zerotier-One"
+curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/main/doc/contact%40zerotier.com.gpg' | gpg --import && \
+if z="$(curl -s 'https://install.zerotier.com/' | gpg)"; then 
+echo "$z" | sudo bash
+fi
+msg_ok "Setup Zerotier-One"
+
+msg_info "Setting up UI"
+curl -O https://s3-us-west-1.amazonaws.com/key-networks/deb/ztncui/1/x86_64/ztncui_0.8.14_amd64.deb
+dpkg -i ztncui_0.8.14_amd64.deb
+sh -c "echo ZT_TOKEN=$(cat /var/lib/zerotier-one/authtoken.secret) > /opt/key-networks/ztncui/.env"
+echo HTTPS_PORT=3443 >> /opt/key-networks/ztncui/.env
+echo NODE_ENV=production >> /opt/key-networks/ztncui/.env
+chmod 400 /opt/key-networks/ztncui/.env
+chown ztncui:ztncui /opt/key-networks/ztncui/.env
+systemctl restart ztncui
+msg_ok "Done setting up UI."
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
