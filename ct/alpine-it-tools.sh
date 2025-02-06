f5a73a54490674fb2789b612fb2d1016f4002a4a
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-02-06 16:23:44 +0100

    Fix: Alpine IT-Tools Update (#2067)
    * Fix: Alpine ITTools Update
    
    * remove wget

--- ct/alpine-it-tools.sh
+++ ct/alpine-it-tools.sh
@@ -29,27 +29,28 @@ function update_script() {
     check_container_storage
     check_container_resources
 
-    if [[ ! -d /usr/share/nginx/html ]]; then
-        msg_error "No ${APP} Installation Found!"
-        exit
-    fi
+if [ ! -d /usr/share/nginx/html ]; then
+    msg_error "No ${APP} Installation Found!"
+    exit 1
+fi
 
-    RELEASE=$(curl -s https://api.github.com/repos/CorentinTh/it-tools/releases/latest | grep '"tag_name":' | cut -d '"' -f4)
-    if [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]] || [[ ! -f /opt/${APP}_version.txt ]]; then
-        DOWNLOAD_URL="https://github.com/CorentinTh/it-tools/releases/download/${RELEASE}/it-tools-${RELEASE#v}.zip"
-        msg_info "Updating ${APP} LXC"
-        curl -fsSL -o it-tools.zip "$DOWNLOAD_URL"
-        mkdir -p /usr/share/nginx/html
-        rm -rf /usr/share/nginx/html/*
-        unzip -q it-tools.zip -d /tmp/it-tools
-        cp -r /tmp/it-tools/dist/* /usr/share/nginx/html
-        rm -rf /tmp/it-tools
-        rm -f it-tools.zip
-        msg_ok "Updated Successfully"
-    else
-        msg_ok "No update required. ${APP} is already at ${RELEASE}"
-    fi
-    exit
+RELEASE=$(curl -s https://api.github.com/repos/CorentinTh/it-tools/releases/latest | grep '"tag_name":' | cut -d '"' -f4)
+if [ "${RELEASE}" != "$(cat /opt/${APP}_version.txt 2>/dev/null)" ] || [ ! -f /opt/${APP}_version.txt ]; then
+    DOWNLOAD_URL="https://github.com/CorentinTh/it-tools/releases/download/${RELEASE}/it-tools-${RELEASE#v}.zip"
+    msg_info "Updating ${APP} LXC"
+    curl -fsSL -o it-tools.zip "$DOWNLOAD_URL"
+    mkdir -p /usr/share/nginx/html
+    rm -rf /usr/share/nginx/html/*
+    unzip -q it-tools.zip -d /tmp/it-tools
+    cp -r /tmp/it-tools/dist/* /usr/share/nginx/html
+    rm -rf /tmp/it-tools
+    rm -f it-tools.zip
+    msg_ok "Updated Successfully"
+else
+    msg_ok "No update required. ${APP} is already at ${RELEASE}"
+fi
+
+exit 0
 }
 
 start
