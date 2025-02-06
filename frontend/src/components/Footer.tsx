d71ef779ecee7ab33c804c3b477d5dd69710d6b2
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2025-02-06 19:10:57 +0100

    Website: Refactor Footer for improved layout and styling consistency (#2107)

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,34 +1,38 @@
 import { basePath } from "@/config/siteConfig";
 import Link from "next/link";
 import { FileJson, Server, ExternalLink } from "lucide-react";
+import { buttonVariants } from "./ui/button";
+import { cn } from "@/lib/utils";
 
 export default function Footer() {
   return (
-    <div className="supports-backdrop-blur:bg-background/90 mt-auto flex border-t border-border bg-background/40 py-6 backdrop-blur-lg">
-      <div className="mx-6 w-full max-w-7xl flex justify-between text-xs sm:text-sm text-muted-foreground">
-        <div>
-          Website built by the community. The source code is available on{" "}
-          <Link
-            href={`https://github.com/community-scripts/${basePath}`}
-            target="_blank"
-            rel="noreferrer"
-            className="font-semibold underline-offset-2 duration-300 hover:underline"
-            data-umami-event="View Website Source Code on Github"
-          >
-            GitHub
-          </Link>
-          .
+    <div className="supports-backdrop-blur:bg-background/90 mt-auto border-t w-full flex justify-between border-border bg-background/40 py-6 backdrop-blur-lg">
+      <div className="mx-6 w-full flex justify-between text-xs sm:text-sm text-muted-foreground">
+        <div className="flex items-center">
+          <p>
+            Website built by the community. The source code is available on{" "}
+            <Link
+              href={`https://github.com/community-scripts/${basePath}`}
+              target="_blank"
+              rel="noreferrer"
+              className="font-semibold underline-offset-2 duration-300 hover:underline"
+              data-umami-event="View Website Source Code on Github"
+            >
+              GitHub
+            </Link>
+            .
+          </p>
         </div>
-        <div className="flex gap-4">
+        <div className="sm:flex hidden">
           <Link
             href="/json-editor"
-            className="flex items-center gap-2 text-primary hover:underline"
+            className={cn(buttonVariants({ variant: "link" }), "text-muted-foreground flex items-center gap-2")}
           >
             <FileJson className="h-4 w-4" /> JSON Editor
           </Link>
           <Link
             href="/data"
-            className="flex items-center gap-2 text-primary hover:underline"
+            className={cn(buttonVariants({ variant: "link" }), "text-muted-foreground flex items-center gap-2")}
           >
             <Server className="h-4 w-4" /> API Data
           </Link>
