62425bc51499f1bc94f83ca923ce7c7c47f00d60
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-02-12 11:35:23 +0100

    add more deps

--- install/paperless-ai-install.sh
+++ install/paperless-ai-install.sh
@@ -18,7 +18,10 @@ $STD apt-get install -y \
 	sudo \
 	mc \
 	gpg \
-  make
+  make \
+  gcc \
+  g++ \
+  build-essential
 msg_ok "Installed Dependencies"
 
 msg_info "Setting up Node.js Repository"
