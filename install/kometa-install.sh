2a91a5d2e9d5d035fcb06d602a97bdf7e1c25dbd
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-02-12 22:45:28 +0100

    Fix: Kometa - Fix wrong web site address (#2318)
    * fix web address
    
    * fix another bad web address

--- install/kometa-install.sh
+++ install/kometa-install.sh
@@ -3,7 +3,7 @@
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: Slaviša Arežina (tremor021)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-# Source: https://github.com/Snawoot/kometa
+# Source: https://github.com/Kometa-Team/Kometa
 
 source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
 color
