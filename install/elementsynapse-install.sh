d159e47a62a07c875ef21c1e02ee9ea587171164
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-02-13 20:40:31 +0100

    Fix: Element Synapse - Fixed server listening on both localhost and 0.0.0.0 (#2376)
    * fixed server listening on both localhost and 0.0.0.0
    
    * fix typo

--- install/elementsynapse-install.sh
+++ install/elementsynapse-install.sh
@@ -26,12 +26,13 @@ msg_ok "Installed Dependencies"
 read -p "Please enter the name for your server: " servername
 
 msg_info "Installing Element Synapse"
-wget -O /usr/share/keyrings/matrix-org-archive-keyring.gpg https://packages.matrix.org/debian/matrix-org-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/matrix-org-archive-keyring.gpg] https://packages.matrix.org/debian/ $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/matrix-org.list
+wget -q -O /usr/share/keyrings/matrix-org-archive-keyring.gpg https://packages.matrix.org/debian/matrix-org-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/matrix-org-archive-keyring.gpg] https://packages.matrix.org/debian/ $(lsb_release -cs) main" >/etc/apt/sources.list.d/matrix-org.list
 $STD apt-get update
 echo "matrix-synapse-py3 matrix-synapse/server-name string $servername" | debconf-set-selections
 echo "matrix-synapse-py3 matrix-synapse/report-stats boolean false" | debconf-set-selections
 $STD apt-get install matrix-synapse-py3 -y
+systemctl stop matrix-synapse
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/matrix-synapse/homeserver.yaml
 sed -i 's/'\''::1'\'', //g' /etc/matrix-synapse/homeserver.yaml
 systemctl enable -q --now matrix-synapse
