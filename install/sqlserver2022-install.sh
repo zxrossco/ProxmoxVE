031aefe05a6f4ca72dedb1fa4ee77e240890df14
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-02-18 20:31:28 +0100

    Fix: SQL Server 2022 | GPG & Install (#2476)

--- install/sqlserver2022-install.sh
+++ install/sqlserver2022-install.sh
@@ -2,10 +2,9 @@
 
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: Kristian Skov
-# License: MIT
-# https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 
-source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
 color
 verb_ip6
 catch_errors
@@ -15,33 +14,30 @@ update_os
 
 msg_info "Installing Dependencies"
 $STD apt install -y \
-	curl \
+  curl \
   mc \
   sudo \
-	gpg \
-	coreutils
+  gpg \
+  coreutils
 msg_ok "Installed Dependencies"
 
 msg_info "Setup SQL Server 2022"
-curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
-curl -fsSL https://packages.microsoft.com/config/ubuntu/22.04/mssql-server-2022.list | sudo tee /etc/apt/sources.list.d/mssql-server-2022.list
-#$STD curl https://packages.microsoft.com/keys/microsoft.asc | tee /etc/apt/trusted.gpg.d/microsoft.asc
-#$STD curl -fsSL https://packages.microsoft.com/config/ubuntu/22.04/mssql-server-2022.list | tee /etc/apt/sources.list.d/mssql-server-2022.list
-$STD apt-get clean *
+curl -s https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc > /dev/null
+curl -fsSL https://packages.microsoft.com/config/ubuntu/22.04/mssql-server-2022.list | tee /etc/apt/sources.list.d/mssql-server-2022.list > /dev/null
 $STD apt-get update -y
-apt-get install -y mssql-server
+$STD apt-get install -y mssql-server
 msg_ok "Setup Server 2022"
 
 msg_info "Installing SQL Server Tools"
 export DEBIAN_FRONTEND=noninteractive
 export ACCEPT_EULA=Y
-curl https://packages.microsoft.com/keys/microsoft.asc | tee /etc/apt/trusted.gpg.d/microsoft.asc
-curl https://packages.microsoft.com/config/ubuntu/22.04/prod.list | tee /etc/apt/sources.list.d/mssql-release.list
+curl -s https://packages.microsoft.com/keys/microsoft.asc | tee /etc/apt/trusted.gpg.d/microsoft.asc > /dev/null
+curl -fsSL https://packages.microsoft.com/config/ubuntu/22.04/prod.list | tee /etc/apt/sources.list.d/mssql-release.list > /dev/null
 $STD apt-get update
 $STD apt-get install -y -qq \
   mssql-tools18 \
   unixodbc-dev
-echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bash_profile
+echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >>~/.bash_profile
 source ~/.bash_profile
 msg_ok "Installed SQL Server Tools"
 
@@ -53,7 +49,7 @@ else
 fi
 
 msg_info "Start Service"
-systemctl enable -q --now mssql-server 
+systemctl enable -q --now mssql-server
 msg_ok "Service started"
 
 motd_ssh
