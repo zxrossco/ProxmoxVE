d1e0c2d1643da5a2791d47471ea80af633dedc8a
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-02-19 15:26:20 +0100

    Fix: Kometa - fixed successful setup message and added info to json (#2495)
    * Fix the installation complete message
    
    * Added info in json

--- ct/kometa.sh
+++ ct/kometa.sh
@@ -73,5 +73,5 @@ description
 
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
-echo -e "${INFO}${YW} Access it using the following URL:${CL}"
-echo -e "${TAB}${GATEWAY}${BGN}http://${IP}${CL}"
+echo -e "${INFO}${YW} Access the LXC at following IP address:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}${IP}${CL}"
