2dbd03b74c3a43aef2460f1936651e7ffa8ca032
Dave <80132827+Dave-code-creater@users.noreply.github.com>
2025-02-24 03:10:43 -0500

    New Script: Jupyter Notebook (#2561)
    * Add jupyter notebook script, install script and json
    
    * Add jupyter notebook script, install script and json
    
    * Update jupyternotebook.sh
    
    * Update jupyternotebook-install.sh
    
    * Update jupyternotebook.sh
    
    * Update jupyternotebook-install.sh
    
    * Update jupyternotebook.json
    
    ---------
    
    Co-authored-by: Dat Ta <datta@macmini.homeserver.local>
    Co-authored-by: CanbiZ <47820557+MickLesk@users.noreply.github.com>

---
+++ ct/jupyternotebook.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: Dave-code-creater (Tan Dat, Ta)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://jupyter.org/
+
+APP="Jupyter Notebook"
+var_tags="ai;dev-tools"
+var_cpu="2"
+var_ram="2048"
+var_disk="4"
+var_os="ubuntu"
+var_version="24.04"
+var_unprivileged="1"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+    header_info
+    check_container_storage
+    check_container_resources
+    
+    msg_info "Updating ${APP} LXC"
+    apt-get update &>/dev/null
+    apt-get install -y upgrade &>/dev/null
+    pip3 install jupyter --upgrade &>/dev/null
+    msg_ok "Updated Successfully"
+    exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8888${CL}"
---
+++ install/jupyternotebook-install.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: [Dave-code-creater (Tan Dat, Ta)]
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: [https://jupyter.org/]
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y \
+    curl \
+    sudo \
+    mc \
+    python3 \
+    python3-pip
+msg_ok "Installed Dependencies"
+
+msg_info "Setting up Jupyter Notebook"
+$STD pip3 install jupyter
+msg_ok "Setup Jupyter Notebook"
+
+msg_info "Creating Service"
+cat <<EOF >/etc/systemd/system/jupyternotebook.service
+[Unit]
+Description=Jupyter Notebook Server
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root
+Restart=always
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable -q --now jupyternotebook
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
