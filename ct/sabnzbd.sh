c12470ff125b279fff1c6278753274a64dd4599a
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-02-26 19:20:48 +0100

    Fix: SABnzbd - Removed few artefacts in the code preventing the update (#2670)
    * Fix copy files to folder
    
    * Fix untaring the archive

--- ct/sabnzbd.sh
+++ ct/sabnzbd.sh
@@ -31,8 +31,8 @@ function update_script() {
    if [[ ! -f /opt/${APP}_version.txt ]] || [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]]; then
       msg_info "Updating $APP to ${RELEASE}"
       systemctl stop sabnzbd.service
-      $STD tar zxvf <(curl -fsSL https://github.com/sabnzbd/sabnzbd/releases/download/$RELEASE/SABnzbd-${RELEASE}-src.tar.gz)
-      $STD \cp -r SABnzbd-${RELEASE}/* /opt/sabnzbd
+      tar zxvf <(curl -fsSL https://github.com/sabnzbd/sabnzbd/releases/download/$RELEASE/SABnzbd-${RELEASE}-src.tar.gz)
+      cp -rf SABnzbd-${RELEASE}/* /opt/sabnzbd
       rm -rf SABnzbd-${RELEASE}
       cd /opt/sabnzbd
       $STD python3 -m pip install -r requirements.txt
