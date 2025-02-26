981baed9d38fc0f849d65d9a2b116a923e4da90d
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-02-26 09:50:13 +0100

    PiHole: Fix Unbound sed for DNS (#2647)

--- install/pihole-install.sh
+++ install/pihole-install.sh
@@ -141,7 +141,7 @@ server=8.8.8.8
 server=8.8.4.4
 EOF
 
-  sed -i -E "s|^(upstreams =).*|\1 [\"127.0.0.1#5335\", \"8.8.4.4\"]|" /etc/pihole/pihole.toml
+  sed -i -E 's|^(\s*upstreams =).*|\1 ["127.0.0.1#5335", "8.8.4.4"]|' /etc/pihole/pihole.toml
   systemctl enable -q --now unbound
   systemctl restart pihole-FTL.service
   msg_ok "Installed Unbound"
