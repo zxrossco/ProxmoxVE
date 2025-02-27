0871b9c84ca6622ad60e23946002f3ca15b44ea1
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-02-27 13:13:56 +0100

    Change key to hex32 (#2709)

--- install/outline-install.sh
+++ install/outline-install.sh
@@ -46,7 +46,7 @@ $STD apt-get install -y postgresql-16
 DB_NAME="outline"
 DB_USER="outline"
 DB_PASS="$(openssl rand -base64 18 | tr -dc 'a-zA-Z0-9' | cut -c1-13)"
-SECRET_KEY="$(openssl rand -base64 32 | tr -dc 'a-zA-Z0-9' | cut -c1-32)"
+SECRET_KEY="$(openssl rand -hex 32)"
 $STD sudo -u postgres psql -c "CREATE ROLE $DB_USER WITH LOGIN PASSWORD '$DB_PASS';"
 $STD sudo -u postgres psql -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER ENCODING 'UTF8' TEMPLATE template0;"
 $STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET client_encoding TO 'utf8';"
